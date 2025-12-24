7a576439a73d0c714932be3f1ac6e03c37c8f645
Asim <asimokon@yahoo.com>
2025-12-24 16:14:51 +0100

    series  model added

--- series/models.py
+++ series/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Series(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    thumbnail = models.ImageField(upload_to='series/', blank=True, null=True)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Series"
+        ordering = ['-start_date']
+
+    def __str__(self):
+        return self.title
