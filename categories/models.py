257650dea1f0b9ae106411612a38dbae15061f3a
Asim <asimokon@yahoo.com>
2025-12-25 23:05:30 +0100

    categories  model added

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+# Create many-to-many relationship
+# Add this to sermons/models.py
+class Sermon(models.Model):
+  
+    categories = models.ManyToManyField('categories.Category', blank=True)
