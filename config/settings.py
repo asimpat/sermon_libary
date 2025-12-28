4a681ca84e3275de11f569c45daf45d4c5f43993
Asim <asimokon@yahoo.com>
2025-12-28 20:37:47 +0100

    migration

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'authentication',
-    'sermons',
+    'sermon',
     'series',
     'categories',
 ]
@@ -85,10 +85,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sermon_library_db',
+        'USER': 'sermon_user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
